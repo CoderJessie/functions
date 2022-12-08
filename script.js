8a8ffd43c6ba3cb3b63567bb2e1591854d44a354
Ogbondah Jessica <ogbondahj@gmail.com>
2022-12-09 00:55:46 +0100

    functions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -26,3 +27,14 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+*/
+
+const flight = 'LH234';
+const jonas = {
+	name: 'Jonas Schmedtmann',
+	passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {};
+
+checkIn(flight, jonas);
