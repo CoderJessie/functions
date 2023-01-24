e23914141f40137c3722f77acb9de697ee6b88d0
Ogbondah Jessica <ogbondahj@gmail.com>
2023-01-24 17:02:34 +0100

    default parameters

--- script.js
+++ script.js
@@ -2,7 +2,15 @@
 
 const bookings = [];
 
-const createBooking = function (flightNum, numPassengers, price) {
+const createBooking = function (
+	flightNum,
+	numPassengers = 1,
+	price = 199 * numPassengers
+) {
+	// ES5
+	// numPassengers = numPassengers || 1;
+	// price = price || 199;
+
 	const booking = {
 		flightNum,
 		numPassengers,
@@ -13,3 +21,8 @@ const createBooking = function (flightNum, numPassengers, price) {
 };
 
 createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+createBooking('LH123', undefined, 1000);
