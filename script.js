d77ecc2225ac121a23612084923e5bd40318f1a7
Ogbondah Jessica <ogbondahj@gmail.com>
2023-02-04 22:03:03 +0100

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -26,3 +27,25 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+*/
+
+// How Passing Argument Works: VALUE VS REFERENCE
+const flight = 'LH234';
+const jonas = {
+	name: 'Jonas Schmedtmann',
+	passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+	flightNum = 'LH999';
+	passenger.name = 'Mr. ' + passenger.name;
+
+	if (passenger.passport === 24739479284) {
+		alert('Check in');
+	} else {
+		alert('Wrong passport!');
+	}
+};
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
