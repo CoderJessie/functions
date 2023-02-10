487d741d089474c02feecd4ef94e5264aa50878d
Ogbondah Jessica <ogbondahj@gmail.com>
2023-02-10 09:05:54 +0100

    first commit

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
 */
 
+/*
 // How Passing Argument Works: VALUE VS REFERENCE
 const flight = 'LH234';
 const jonas = {
@@ -59,3 +60,24 @@ const newPassport = function (person) {
 };
 newPassport(jonas);
 checkIn(flight, jonas);
+*/
+
+// Functions Accepting Callback Functions
+const oneWord = function (str) {
+	return str.replaceAll('').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+	const [first, ...others] = str.split(' ');
+	return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order Function
+const transformer = function (str, fn) {
+	console.log(`Original string: ${str}`);
+	console.log(`Transformed string: ${fn(str)}`);
+
+	console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
